719f20687dc7458347134374ea3ae2d102c1954e
Ilya <Korobicin@live.com>
2026-09-08 21:35:53 +0300

    Connect Restaurant Academy demo to Firebase

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,9 @@
 // Restaurant Academy Firebase only. Never point this file at Slow or any other project.
-// Paste the exact Web App config from Firebase Console after registering "Restaurant Academy Web".
 window.RA_FIREBASE_CONFIG = {
-  apiKey: '',
+  apiKey: 'AIzaSyDUukFBx9Af1_7XIAr2EIGoRB107NBg1wY',
   authDomain: 'restaurant-academy-demo.firebaseapp.com',
   projectId: 'restaurant-academy-demo',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: ''
+  storageBucket: 'restaurant-academy-demo.firebasestorage.app',
+  messagingSenderId: '662828355321',
+  appId: '1:662828355321:web:8af332319a112baae75c28'
 };
